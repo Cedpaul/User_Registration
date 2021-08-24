01db4448840dbf36daeacdb70d23ba8973dff465
Cedrick Paul C. Pangan <cedrickpangan29@gmail.com>
2021-08-24 17:19:24 +0800

    working server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const users = require('./routes/api/routes.user');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db)
+.then(() => console.log('MongoDB Connected'))
+.catch((error) => console.log(error));
+
+app.use('/api/users', users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
