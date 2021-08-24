12edd8f8b83f961cd40fb7b12de80e58f692ff93
Cedrick Paul C. Pangan <cedrickpangan29@gmail.com>
2021-08-24 17:19:10 +0800

    working routes

---
+++ routes/api/routes.user.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../../models/models.user');
+
+router.get('/', (req, res) => {
+    User.find()
+    .then((users) => res.json(users))
+    .catch((error) => console.log(error))
+});
+
+router.post('/', (req, res) => {
+    const newUser = new User({
+        firstName: req.body.firstName,
+        middleName: req.body.middleName,
+        lastName: req.body.lastName,
+        age: req.body.age,
+        stAddress: req.body.stAddress,
+        city: req.body.city,
+        phoneNumber: req.body.phoneNumber
+    });
+
+    newUser.save().then((user) => res.json(user)).catch((error) => console.log(error));
+});
+
+
+module.exports = router;
