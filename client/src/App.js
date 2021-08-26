678017791bec118eb57bb77ee82f99c48afca02b
Cedrick Paul C. Pangan <cedrickpangan29@gmail.com>
2021-08-26 18:10:54 +0800

    update route

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Form from './components/form';
+import UserList from './components/UserList';
+
+import NavBar from './components/navBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+         <div className="App">
+          <NavBar/>
+        </div>
+
+        <Route path="/" exact
+               render={() => (
+                <>
+                  <Form />
+                </>
+              )}
+          />
+          
+        <Route path="/list" exact
+               render={() => (
+                <>
+                  <UserList />
+                </>
+              )}
+          />
+    </Router>
+   
   );
 }
 
